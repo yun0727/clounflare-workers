7268894d9b60b0067ec369516d1f56b34aae0b49
yun <austru00@gmail.com>
2024-02-16 21:32:05 +0900

    23.3 Workers KV

--- workers-visitor/src/index.ts
+++ workers-visitor/src/index.ts
@@ -1,7 +1,7 @@
 
 export interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
+	DB: KVNamespace
 	//
 	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
 	// MY_DURABLE_OBJECT: DurableObjectNamespace;
@@ -23,6 +23,7 @@ export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const url = new URL(request.url);
 		if (url.pathname === "/"){
+			await env.DB.put("hello","how are you?")
 			return new Response(home,{
 				headers:{
 					"Content-Type" :"text/html;charset=utf-8"
